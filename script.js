c8c54856da94008f8e758a83da274dbddb915536
Mário Lanza <m.lanza.faria@gmail.com>
2023-01-04 22:46:42 -0300

    add readme

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function toggleMode() {
   } else {
     //se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute("alt", "Imagem do profile")
+    img.setAttribute("alt", "Imagem do profilegit")
   }
 
   /* Toggle light
